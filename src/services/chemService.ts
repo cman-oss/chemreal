1ea8fd7bbe27254137bb330922eed48a7fd5ddef
cman-oss <website1232@outlook.com>
2026-04-02 14:32:28 -0400

    Fix import path for firebase in chemService

--- src/services/chemService.ts
+++ src/services/chemService.ts
@@ -1,5 +1,5 @@
 import { collection, addDoc, query, where, onSnapshot, doc, deleteDoc, updateDoc, getDocs } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db } from './firebase';
 import { handleFirestoreError, OperationType } from '../lib/firestore-errors';
 
 export interface InventoryItem {
